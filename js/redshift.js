e498249489ff137cb582b298b5e54a2974e099cd
syntaxval <syntaxval@users.noreply.github.com>
2017-06-22 16:31:04 +1000

    Added uri.js, refactoring.

--- js/redshift.js
+++ js/redshift.js
@@ -3,29 +3,22 @@ jshint esversion: 6
 */
 
 /*
-globals $, requirejs, StellarSdk
+globals requirejs, StellarSdk
 */
 
 
 requirejs.config({
     baseUrl: "js/lib",
     paths: {
-        // the left side is the module ID,
-        // the right side is the path to
-        // the jQuery file, relative to baseUrl.
-        // Also, the path should NOT include
-        // the ".js" file extension. This example
-        // is using jQuery 1.9.0 located at
-        // js/lib/jquery-1.9.0.js, relative to
-        // the HTML page.
         jquery: "jquery-3.2.1.min"
     }
 });
 
 requirejs([
     "jquery",
-    "stellar-sdk.min",
-], function ($) {
+    "uri/URI",
+    "stellar-sdk.min"
+], function ($, URI) {
 
   	"use strict";
 
@@ -39,6 +32,14 @@ requirejs([
         if ($.redshift === undefined) {
             $.redshift = {};
         }
+
+        /* Setup 'conf' namespace */
+        if ($.redshift.conf === undefined) {
+            $.redshift.conf = {
+                url: {}
+            };
+        }
+
         /* Setup 'utils' namespace */
         if ($.redshift.lib === undefined) {
             $.redshift.lib = {
@@ -46,32 +47,95 @@ requirejs([
             };
         }
 
+        $.redshift.conf.url.strnet = "https://horizon.stellar.org";
+        $.redshift.conf.url.ticker = "https://api.cryptonator.com/api/ticker/xlm-eur";
+        $.redshift.conf.account = "GDQXGA5JF2S4QLA55TBWZLX666INIOPRY52V2PAAKOOI7XU4P47TLLJ4";
+
+
+
+
         /************************ FUNCTION DEFINITIONS ************************/
 
+        $.redshift.lib.addEvent = function (selector, eventType, f) {
+            $(selector).each(function () {
+                $(this).off(eventType).on(eventType, f);
+            });
+        };
+
         $.redshift.lib.printAccountSummary = function (accountNumber) {
-            let server = new StellarSdk.Server("https://horizon.stellar.org");
-            $(".xlm-account-number").text(accountNumber);
-            $.when(server.loadAccount(accountNumber).then(function(account) {
-                let summary = [];
-                account.balances.forEach(function(balance) {
-                    summary.push({
-                        type: balance.asset_type,
-                        balance: balance.balance
+            let server = new StellarSdk.Server($.redshift.conf.url.strnet);
+            let d_getAccountInfo = function (acctNum) {
+                return server.loadAccount(acctNum).then(function(account) {
+                    let summary = [];
+                    account.balances.forEach(function(balance) {
+                        summary.push({
+                            type: balance.asset_type,
+                            balance: balance.balance
+                        });
                     });
+                    return summary;
+                }, function (err) {
+                    console.log("Error accessing account info. [" + err.message + "]");
                 });
-                return summary;
-            }), $.ajax("https://api.cryptonator.com/api/ticker/xlm-eur").then(function (erate) {
-                return erate;
-            })).done(function (result, erate) {
-                $(".xlm-balance").text(result[0].balance);
-                $(".xlm-balance-eur").text(
-                    (parseFloat(result[0].balance) * parseFloat(erate.ticker.price)).toFixed(2)
-                );
+            };
+            let d_getExchangeRates = function () {
+                return $.ajax($.redshift.conf.url.ticker).then(function (erate) {
+                    return erate;
+                }, function (err) {
+                    console.log("Error getting exchange rates. [" + err.message + "]");
+                    console.log(err.message);
+                });
+            };
+
+
+
+            $.when(
+
+                // load account information
+                d_getAccountInfo(accountNumber),
+                // get exchange rates
+                d_getExchangeRates()
+
+            ).done(function (result, erate) {
+
+                if (result !== undefined) {
+                    $(".xlm-account-number").text(accountNumber);
+                    $(".xlm-balance").text(result[0].balance);
+                    $(".xlm-balance-eur").text(
+                        (parseFloat(result[0].balance) * parseFloat(erate.ticker.price)).toFixed(2)
+                    );
+                }
+
             });
         };
 
 
         /****************************** RUNTIME *******************************/
+        $(document).ready(function() {
+            $.redshift.lib.addEvent(
+                "#button-update",
+                "click",
+                function (e) {
+                    $.redshift.lib.printAccountSummary($.redshift.conf.account);
+                }
+            );
+
+            $.redshift.lib.addEvent(
+                "#test-net-switch",
+                "change",
+                function (element) {
+                    let uri = new URI($.redshift.conf.url.strnet);
+                    if (element.currentTarget.checked) {
+                        uri.subdomain("test-horizon");
+                        $("body").css("background-color", "#ededed");
+                    } else {
+                        uri.subdomain("horizon");
+                        $("body").css("background-color", "#fff");
+                    }
+                    $.redshift.conf.url.strnet = uri;
+                }
+            );
+        });
     });
 
 });
